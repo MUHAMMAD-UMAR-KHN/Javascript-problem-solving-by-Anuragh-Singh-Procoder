5d936e5aaa41607293489687e5715d77f8aac9f3
MUHAMMAD-UMAR-KHN <uk1555464@gmail.com>
2025-11-07 18:30:38 +0500

    first day challanges completed

---
+++ app.js
@@ -0,0 +1,111 @@
+// var number = prompt("enter the number: ");
+// for(let i = 1; i < number;i++){
+//     console.log(i);
+// }
+
+// ================ 🙂 Problem 1 ======================
+// var btn = document.getElementById("btn");
+// btn.addEventListener("click", () => {
+//     var number = document.getElementById("nums").value;
+//     for (let i = 1; i < number; i++) {
+//         console.log(i)
+//     }
+// })
+
+// ================== 😀 Problem 2 ======================
+
+// var number = 10;
+// for (let i = 1; i <= number; i++) {
+//     console.log(number - i + 1)
+// }
+
+
+//=============== 😃 problem 3 =========================
+
+// var number = 20;
+// for(let i = 1; i <= number; i++){
+//   if(i % 2 == 0){
+//       console.log(i)
+//   }
+// }
+
+// ================😎 problem 4 =====================
+
+// var inputNumber = 5;
+// var number = 0;
+// for(let i = 1; i<= inputNumber; i++){
+//     number = number + i;
+// }
+// console.log(`the sum of number from 1 to ${inputNumber} is "${number}"`);
+
+
+// ==============😄 problem 5 =====================
+
+// var inputNumber = 5;
+// var number = 1;
+// for(let i = 1; i<= inputNumber; i++){
+//     number = number * i;
+// }
+// console.log(`the factorial of number ${inputNumber} is "${number}"`);
+
+
+// =============== 😍 Problem 6 ======================
+// var number = 20;
+// var evenSum = 0;
+// for(let i = 1; i <= number; i++){
+//   if(i % 2 == 0){
+//       evenSum += i;
+//   }
+// }
+// console.log(`The sum of even numbers is ${evenSum}`);
+
+// ==================== 😎 Problem 7 =================
+
+// var number = 10;
+// for(let i = 1; i <= number;i++){
+//     var sqNum = i*i;
+//     console.log(`the square of "${i}" is ${sqNum}.`);
+// }
+
+// =============== 🏠 Home work Problem 1 ====================
+
+// var number =  100;
+// for(let i = 1; i <= number; i++){
+//     if(i % 3 == 0 && i % 5 == 0){
+//         console.log(`${i} is the number which is divisible by 3 and 5`);
+//     }
+// }
+
+// =============== 🏠 Home work Problem 2 ====================
+
+// var number =  100;
+// var sumOdd = 0;
+// for(let i = 1; i <= number; i++){
+//     if(i % 2 != 0){
+//         sumOdd += i;
+//     }
+// }
+// console.log(`The sum of all the odd number from 1 to ${number} is ${sumOdd}`);
+
+// this is second approach to calculate the sum of odd numbers by using formula of sum of odd numbers which is :- n*n or square of n.
+
+// var numb = 9;
+// var oddNumbersSum = Math.pow(numb,2);
+// console.log(oddNumbersSum);
+
+// =============== 🏠 Home work Problem 3 ====================
+
+// var number = 10;
+// for(let i = 1; i<= number; i++){
+//     console.log(`The cube of the ${i} is ${i**3}`);
+// }
+
+// =============== 🏠 Home work Problem 4 ====================
+
+// var number = 50;
+// for(let i = 1; i<= number; i++){
+//     var squares = i**2;
+//     if (squares % 2 == 0 && squares <= 100) {
+//         console.log(`The ${squares} is the square of ${i} and fulfill the both conditions even and perfect square.`)
+//     }
+// }
